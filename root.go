cb0e6c74bd03456b3f8d8926866a1c2591aea5fc
Matthias Ernst <www@jungerchorzueri.ch>
2015-10-09 18:18:51 +0200

    Handle /index.html in case anyone has it cached.

--- root.go
+++ root.go
@@ -3,7 +3,10 @@ package main
 import "net/http"
 
 func init() {
-  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+  f := func (w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "/konzerte/warum/flyer.html", http.StatusFound)
-  })
+  }
+  http.HandleFunc("/", f);
+  http.HandleFunc("/index.html", f);
 }
+
