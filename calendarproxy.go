6323906b633d7b629e1651d4a24a6f3c3dfccdc3
mernst-github <mernst-github@mernst.org>
2015-11-07 12:47:36 +0100

    Update calendarproxy.go
    Fetch the ICS instead of redirecting. Calendar might not like fetches from bot.

--- calendarproxy.go
+++ calendarproxy.go
@@ -1,12 +1,25 @@
 package main
 
-import "net/http"
+import ("io"
+        "net/http"
+        "appengine"
+        "appengine/urlfetch")
 
 func init() {
   http.HandleFunc("/calendar/", func (w http.ResponseWriter, r *http.Request) {
     u := *r.URL
     u.Scheme = "https"
     u.Host = "www.google.com"
-    http.Redirect(w, r, u.String(), http.StatusFound)
+    
+    c := appengine.NewContext(r)
+    client := urlfetch.Client(c)
+    resp, err := client.Get(u.String())
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer resp.Body.Close()
+    w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+    io.Copy(w, resp.Body)
   })
 }
