21d811de4d256a9a221382e4ad2e24eee65843c0
mernst-github <mernst-github@mernst.org>
2016-04-11 23:20:25 +0200

    Update root.go

--- root.go
+++ root.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func init() {
   f := func (w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "/konzerte/warum/flyer.html", http.StatusFound)
+    http.Redirect(w, r, "/konzerte/nacht/index.html", http.StatusFound)
   }
   http.HandleFunc("/", f);
   http.HandleFunc("/index.html", f);
